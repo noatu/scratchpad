30f820a38ed48cc336ddd52582d13dcaf90aaf32
Anton Bilous <oxidate@tuta.io>
2025-09-18 23:41:15 +0300

    proc-macro-workshop debug 8/8
    made it fail sooner if there are attribute errors

--- rust/proc-macro-workshop/debug/src/lib.rs
+++ rust/proc-macro-workshop/debug/src/lib.rs
@@ -2,10 +2,10 @@ use proc_macro::TokenStream;
 use std::collections::HashSet;
 
 use proc_macro2::{Span, TokenStream as TokenStream2};
-use quote::quote;
+use quote::{quote, quote_spanned};
 use syn::{
-    Data, DeriveInput, Error, Fields, Generics, Ident, Meta, PathSegment, Result, Token, TypePath,
-    WherePredicate,
+    Data, DeriveInput, Error, Expr, Fields, Generics, Ident, Lit, LitStr, Meta, PathSegment,
+    Result, Token, Type, TypePath, WherePredicate,
     parse::{Parse, ParseStream, Parser},
     parse_macro_input, parse_quote_spanned,
     punctuated::Punctuated,
@@ -13,6 +13,7 @@ use syn::{
     visit::{self, Visit},
 };
 
+/// Parse the `Ident = TokenStream` that can be found inside `#[attr(name = value)]`.
 struct AttrAssign {
     ident: Ident,
     ts2: TokenStream2,
@@ -27,7 +28,7 @@ impl Parse for AttrAssign {
 }
 
 struct AutomaticBounds {
-    generics: Vec<Ident>,
+    generics: HashSet<Ident>,
     bounds: HashSet<TypePath>,
 }
 
@@ -67,109 +68,118 @@ impl<'ast> Visit<'ast> for AutomaticBounds {
     }
 }
 
-// struct AttrBounds(Punctuated<WherePredicate, Token![,]>);
-
-// impl Parse for AttrBounds {
-//     fn parse(input: ParseStream) -> Result<Self> {
-//         let bound: Ident = input.parse()?;
-//         if bound != "bound" {
-//             return Err(Error::new(bound.span(), "unrecognized attribute"));
-//         }
-//         input.parse::<Token![=]>()?;
-//         Ok(Self(Punctuated::parse_separated_nonempty(input)?))
-//     }
-// }
-
 fn derive_impl(input: DeriveInput) -> Result<TokenStream2> {
-    let Data::Struct(data_struct) = &input.data else {
+    let Data::Struct(data_struct) = input.data else {
         return Err(Error::new(Span::call_site(), "expected a struct"));
     };
-    let Fields::Named(fields) = &data_struct.fields else {
+    let Fields::Named(fields) = data_struct.fields else {
         return Err(Error::new(Span::call_site(), "expected named fields"));
     };
-    let fields = &fields.named;
 
-    let mut bounds = Vec::new();
+    let mut generics = input.generics;
+    let mut auto_bounds = true;
+    let bounds_parser = Punctuated::<WherePredicate, Token![,]>::parse_terminated;
 
-    for attr in &input.attrs {
+    for attr in input.attrs {
         if !attr.path().is_ident("debug") {
             continue;
         }
 
         let AttrAssign { ident, ts2 } = attr.parse_args()?;
 
-        if ident == "bound" {
-            let parser = Punctuated::<WherePredicate, Token![,]>::parse_separated_nonempty;
-            bounds.extend(parser.parse2(ts2)?.into_iter());
-        } else {
+        if ident != "bound" {
             return Err(Error::new(ident.span(), "unknown attribute"));
         }
+
+        generics
+            .make_where_clause()
+            .predicates
+            .extend(bounds_parser.parse2(ts2)?.into_iter());
+        // disable automatic bounds even if bounds are empty
+        auto_bounds = false;
     }
 
-    let auto_inference = bounds.is_empty();
+    let mut field_types: Vec<Type> = Vec::new();
+    let mut field_formats: Vec<(Ident, Option<LitStr>)> = Vec::new();
 
-    let mut impl_fields = Vec::new();
-    let mut auto_bounds = AutomaticBounds::new(&input.generics);
+    for field in fields.named {
+        let ident = field
+            .ident // NOTE: could unwrap though
+            .ok_or_else(|| Error::new(Span::call_site(), "expected named fields"))?;
 
-    for field in fields {
-        let mut field_format = Vec::new();
-        let mut field_bounds = Vec::new();
+        let mut field_auto_bounds = true;
+        let mut format: Option<LitStr> = None;
 
-        for attr in &field.attrs {
+        for attr in field.attrs {
             if !attr.path().is_ident("debug") {
                 continue;
             }
 
-            match &attr.meta {
-                Meta::NameValue(nv) => field_format.push(&nv.value),
+            match attr.meta {
                 Meta::List(_) => {
                     let AttrAssign { ident, ts2 } = attr.parse_args()?;
-                    if ident == "bound" {
-                        let parser =
-                            Punctuated::<WherePredicate, Token![,]>::parse_separated_nonempty;
-                        field_bounds.extend(parser.parse2(ts2)?.into_iter());
-                    } else {
+
+                    if ident != "bound" {
                         return Err(Error::new(ident.span(), "unknown attribute"));
+                    }
+
+                    generics
+                        .make_where_clause()
+                        .predicates
+                        .extend(bounds_parser.parse2(ts2)?.into_iter());
+                    // disable automatic bounds even if bounds are empty
+                    field_auto_bounds = false;
+                }
+                Meta::NameValue(nv) => {
+                    if let Some(format) = format {
+                        let mut err =
+                            Error::new(nv.value.span(), "expected only one format attribute");
+                        err.combine(Error::new(format.span(), "first format attribute"));
+                        return Err(err);
+                    }
+
+                    if let Expr::Lit(lit) = &nv.value
+                        && let Lit::Str(s) = &lit.lit
+                    {
+                        format = Some(s.clone());
+                    } else {
+                        return Err(Error::new(nv.value.span(), "expected a str"));
                     }
                 }
                 Meta::Path(p) => return Err(Error::new(p.span(), "unknown attribute")),
             }
         }
 
-        let ident = field.ident.as_ref().unwrap();
-        let ident_str = ident.to_string();
-
-        if field_format.is_empty() {
-            impl_fields.push(quote!(.field(#ident_str, &self.#ident)));
-        } else {
-            impl_fields.extend(
-                field_format
-                    .into_iter()
-                    .map(|expr| quote!(.field(#ident_str, &format_args!(#expr, &self.#ident)))),
-            );
+        if auto_bounds && field_auto_bounds {
+            field_types.push(field.ty);
         }
-
-        if auto_inference && field_bounds.is_empty() {
-            auto_bounds.visit_type(&field.ty);
-        }
-        bounds.extend(field_bounds);
+        field_formats.push((ident, format));
     }
 
-    bounds.extend(
-        auto_bounds
-            .bounds
-            .into_iter()
-            .map(|tp| parse_quote_spanned! {tp.span()=> #tp: ::std::fmt::Debug}),
+    let mut auto_bounds = AutomaticBounds::new(&generics);
+    for ty in field_types {
+        auto_bounds.visit_type(&ty);
+    }
+    generics.make_where_clause().predicates.extend(
+        auto_bounds.bounds.into_iter().map::<WherePredicate, _>(
+            |tp| parse_quote_spanned! {tp.span()=> #tp: ::std::fmt::Debug},
+        ),
     );
 
     let ident = input.ident;
     let ident_str = ident.to_string();
-
-    let mut generics = input.generics;
-    generics.make_where_clause().predicates.extend(bounds);
     let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
 
+    let impl_fields = field_formats.into_iter().map(|(ident, format)| {
+        let ident_str = ident.to_string();
+        format.as_ref().map_or_else(
+            || quote_spanned!(ident.span()=> .field(#ident_str, &self.#ident)),
+            |s| quote_spanned!(s.span()=> .field(#ident_str, &format_args!(#s, &self.#ident))),
+        )
+    });
+
     Ok(quote! {
+        #[allow(clippy::type_repetition_in_bounds)]
         impl #impl_generics ::std::fmt::Debug for #ident #ty_generics #where_clause {
             fn fmt(&self, fmt: &mut ::std::fmt::Formatter<'_>) -> ::std::fmt::Result {
                 fmt.debug_struct(#ident_str)
@@ -183,18 +193,8 @@ fn derive_impl(input: DeriveInput) -> Result<TokenStream2> {
 #[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
-    // dbg!(input);
     TokenStream::from(match derive_impl(input) {
         Ok(ts2) => ts2,
         Err(e) => e.into_compile_error(),
     })
-
-    // let ident_str = ident.to_string();
-    // for param in &mut generics.params {
-    //     if let GenericParam::Type(type_param) = param
-    //         && !inside_phantom.contains(&type_param.ident.to_string())
-    //     {
-    //         type_param.bounds.push(parse_quote!(::std::fmt::Debug));
-    //     }
-    // }
 }
